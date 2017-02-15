e3bcfd9c51a588a672ecdb55ed797b498133fd2c
Matthew epler <matthewepler@Matthews-MacBook-Pro.local>
2017-02-15 16:27:54 -0500

    working test with questions

--- client/test/index.spec.js
+++ client/test/index.spec.js
@@ -1,17 +1,30 @@
-/* global it */
+/* global describe, it */
 
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const expect = chai.expect
+const mqtt = require('mqtt')
 chai.use(sinonChai)
 
-it('should connect to the broker', function () {
-  const mqtt = require('mqtt')
+describe('mqtt chat test', () => {
   // using known reliable broker
   const client = mqtt.connect('mqtt://broker.hivemq.com:1883')
   const connectionCallback = sinon.spy()
 
-  client.on('connect', connectionCallback)
-  expect(connectionCallback).to.have.been.called
+  it('should connect to the broker', () => {
+    client.on('connect', () => {
+      connectionCallback()
+      expect(connectionCallback).to.have.been.called
+    })
+    client.publish('fortunematthewtest', 'one_more_time')
+  })
 })
+
+// PREVIOUS VERSION
+//
+// client.on('connect', connectionCallback)
+//
+// it('should connect', () => {
+//    expect(connectionCallback).to.have.been.called
+// })
