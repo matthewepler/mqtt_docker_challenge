3141212403b2ba8b6c759bfb4e0ea48155675e43
Matthew epler <matthewepler@Matthews-MacBook-Pro.local>
2017-02-15 14:38:23 -0500

    clean non-working test and index.js

--- client/test/index.spec.js
+++ client/test/index.spec.js
@@ -1,4 +1,4 @@
-/* global describe, it */
+/* global it */
 
 const chai = require('chai')
 const sinon = require('sinon')
@@ -6,21 +6,10 @@ const sinonChai = require('sinon-chai')
 const expect = chai.expect
 chai.use(sinonChai)
 
-describe('MQTT messaging', function () {
+it('should connect to the broker', function () {
   const mqtt = require('mqtt')
-  const client = mqtt.connect('mqtt://localhost:9001')
-
-  client.subscribe('testTopic')
-  const connectCallback = sinon.spy()
-  const messageCallback = sinon.spy()
-  client.on('connect', connectCallback)
-  client.on('message', messageCallback)
-
-  it('should send a string to the broker', function () {
-    const message = 'testing testing 123'
-    client.publish('testTopic', message)
-    expect(messageCallback).to.have.been.called
-    expect(messageCallback).to.have.been.calledWith('testTopic', message)
-  })
+  const client = mqtt.connect('mqtt://broker.hivemq.com:1883')
+  const connectionCallback = sinon.spy()
+  client.on('connect', connectionCallback)
+  expect(connectionCallback).to.have.been.called
 })
-
