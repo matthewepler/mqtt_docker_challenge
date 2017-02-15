09b9cf4e2fd3817e235b0c9637715794735a80a1
Matthew Epler <matthewepler@gmail.com>
2017-02-14 19:01:39 -0500

    shows that the publishing is working and does not show up in the broker logs like i was sure it would godammit

--- client/public/index.js
+++ client/public/index.js
@@ -1,8 +1,14 @@
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://localhost:9001')
 
+client.subscribe('welcome')
+
 client.on('connect', function () {
   console.log(process.env.NAME + ' has connected')
+})
+
+client.on('message', function (topic, message) {
+  console.log(message.toString())
 })
 
 module.exports = {
@@ -10,3 +16,11 @@ module.exports = {
     client.publish('welcome', 'hello', callback)
   }
 }
+
+process.on('SIGINT', function () {
+  client.end()
+})
+
+setTimeout(function () {
+  client.publish('welcome', 'come on')
+}, 5000)
