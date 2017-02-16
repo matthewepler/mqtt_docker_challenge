3dca8846cd274ece887ba1fa9d1a6a0324b0f48c
Adam Goldman <adamgoldman7@gmail.com>
2017-02-16 08:25:34 +0800

    add missing dependencies, convert to es6 class, setup correct unit tests

---
+++ client/public/Client.js
@@ -0,0 +1,20 @@
+import mqtt from 'mqtt'
+
+export default class Client {
+
+  init () {
+    this.client = mqtt.connect('mqtt://localhost:9001')
+    this.client.on('connect', this.onConnect)
+    this.client.on('message', this.onMessage)
+  }
+
+  onConnect () {
+    console.log(process.env.NAME + ' has connected')
+    this.client.subscribe('welcome')
+    this.client.publish('welcome', 'heeey')
+  }
+
+  onMessage (topic, msg) {
+    console.log('received: ' + msg.toString())
+  }
+}
