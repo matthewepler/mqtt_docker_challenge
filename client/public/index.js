d61fd6df1599133fa1709f391dcb607c4c24c611
Matthew Epler <matthewepler@gmail.com>
2017-02-18 08:54:41 -0500

    adds pub/sub to docker broker

--- client/public/index.js
+++ client/public/index.js
@@ -1,6 +1,16 @@
 import mqtt from 'mqtt'
 
 const client = mqtt.connect('mqtt://localhost:9001')
-client.on('connect', () => {
-  console.log('connected')
-})
+client.on('connect', onConnected)
+client.on('message', onMessage)
+
+function onConnected () {
+  console.log('hey there')
+}
+
+function onMessage (topic, msg) {
+  console.log(`msg received on ${topic}: ${msg}`)
+}
+
+client.subscribe('welcome')
+client.publish('welcome', 'hello test')
