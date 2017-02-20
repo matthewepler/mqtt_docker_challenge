f3ea8b12414e4049a60e2084bdafa6b5ef746987
Matthew epler <matthewepler@Matthews-MacBook-Pro.local>
2017-02-20 15:35:36 -0500

    adds multiple clients with userids working properly

--- client/app/src/index.js
+++ client/app/src/index.js
@@ -53,7 +53,7 @@ function updateConnectionStatus () {
   const statusIndicator = document.querySelector('#connection-status')
   statusIndicator.style.backgroundColor = color
 
-  client.publish(currTopic, `[${userId} is now ${connected ? 'connected' : 'disconnected'}]`)
+  client.publish(currTopic, packageMessage(null, `${userId} is ${connected ? 'connected' : 'disconnected'}`))
 }
 
 const inputBox = document.querySelector('#input-box input')
@@ -64,10 +64,17 @@ inputBox.addEventListener('keypress', (event) => {
   }
 })
 
+function packageMessage (uid, msg) {
+  return JSON.stringify({
+    'uid': uid,
+    'msg': msg
+  })
+}
+
 function publishMessage (msg) {
   if (msg && msg.length > 0) {
     console.log(`publishing to ${currTopic}: ${msg}`)
-    client.publish(currTopic, msg, (err) => {
+    client.publish(currTopic, packageMessage(userId, msg), (err) => {
       if (err) {
         // ** TO-DO **
       }
@@ -76,28 +83,31 @@ function publishMessage (msg) {
 }
 
 function addToChatWindow (msg) {
+  const data = JSON.parse(msg)
+  console.log(data)
   const feedList = document.querySelector('#feed-list')
   const messageItem = document.createElement('li')
   messageItem.classList.add('message')
   const messageContent = document.createElement('ul')
-  console.log(msg.charAt(0))
-  if (msg.charAt(0) !== '[') {
+  const messageText = document.createElement('li')
+
+  if (data.uid !== null) {
     const messageId = document.createElement('li')
     messageId.classList.add('user-id')
-    messageId.appendChild(document.createTextNode(userId))
+    messageId.appendChild(document.createTextNode(data.uid))
     messageContent.appendChild(messageId)
+  } else {
+    messageText.style.fontStyle = 'italic'
   }
-  const messageText = document.createElement('li')
+
   messageText.classList.add('message-text')
-  messageText.appendChild(document.createTextNode(msg))
+  messageText.appendChild(document.createTextNode(data.msg))
   messageContent.appendChild(messageText)
   messageItem.appendChild(messageContent)
   feedList.appendChild(messageItem)
 }
 
-// append 'has joined' on connection
-// insertBefore() for each additional message
-// add 'has left' on disconnection
+// send ID as part of message, split on reciept
 //
 // the 2 connection problem
 //      Solution: run webpack with --watch + separate server
