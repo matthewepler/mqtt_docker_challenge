110fbfd06c7356a110707f0191022dbf75013220
Matthew epler <matthewepler@Matthews-MacBook-Pro.local>
2017-02-23 12:47:40 -0500

    adds in-browser testing via index.html in /client

---
+++ client/test/index.tests.js
@@ -0,0 +1,8 @@
+/* global chai, describe, it */
+const expect = chai.expect
+
+describe('Chat tests', function () {
+  it('should be true', function () {
+    expect(document).to.be.okay
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,26 @@
 const { resolve } = require('path')
-const join = require('path').join
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: resolve(__dirname, 'public/index.js'),
+  devtool: 'source-map',
+  entry: resolve(__dirname, 'app/src/index.js'),
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        include: /public/,
-        use: {
-          loader: 'babel-loader'
-        }
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
   },
   output: {
-    path: resolve(__dirname, '/public'),
+    path: resolve(__dirname, 'app/dist/js'),
     filename: 'bundle.js'
   },
   plugins: [
-    // detects changes to html files
-    new HtmlWebpackPlugin({
-      template: resolve(__dirname, 'public/index.html')
-    }),
+    new HtmlWebpackPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.EnvironmentPlugin(['NAME'])
-  ],
+  ]
 }
